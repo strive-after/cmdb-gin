47bf6b700a024b406250185eb1614ee2898f2622
qushuaibo <qushuaibo@qushuaibodeMacBook-Pro.local>
2021-04-22 12:59:40 +0800

    init config

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"gin-moudle/internal/config"
 	"gin-moudle/internal/router"
 	"github.com/gin-gonic/gin"
 )
@@ -13,5 +14,12 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	//加载配置文件
+	_, err = config.InitConfig("../configs/config.toml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	g.Run(":8080")
 }
